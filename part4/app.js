6559ef58787d75cc5a55082301d50baed9f0d16b
RubenArthuys <ruben.arthuys@gmail.com>
2022-06-16 15:55:18 +0200

    code

--- part4/app.js
+++ part4/app.js
@@ -9,13 +9,14 @@ var usersRouter = require('./routes/users');
 var session = require("express-session");
 
 var app = express();
+
 app.use(
   session({
-   secret: 'a4f8071f-c873-4447-8ee2',
-   resave: false,
-   saveUninitialized: false,
+    secret: 'a4f8071f-c873-4447-8ee2',
+    resave: false,
+    saveUninitialized: false,
   })
-  );
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,12 +32,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- part4/routes/index.js
+++ part4/routes/index.js
@@ -2,38 +2,47 @@ var express = require('express');
 var router = express.Router();
 
 var dataBikeVar = [
-  { name : "BIK045" , url : "/images/bike-1.jpg", price : 679 },
-  { name : "ZOOK07" , url : '/images/bike-2.jpg', price : 999 },
-  { name : "TITANS" , url : '/images/bike-3.jpg', price : 799 },
-  { name : "CEWO" , url : '/images/bike-4.jpg', price : 1300 },
-  { name : "AMIG39" , url : '/images/bike-5.jpg', price : 479 },
-  { name : "LIK099" , url : '/images/bike-6.jpg', price : 869 }    
+  { name: "BIK045", url: "/images/bike-1.jpg", price: 679 },
+  { name: "ZOOK07", url: '/images/bike-2.jpg', price: 999 },
+  { name: "TITANS", url: '/images/bike-3.jpg', price: 799 },
+  { name: "CEWO", url: '/images/bike-4.jpg', price: 1300 },
+  { name: "AMIG39", url: '/images/bike-5.jpg', price: 479 },
+  { name: "LIK099", url: '/images/bike-6.jpg', price: 869 }
 ]
 
 
 // Home page
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  // console.log(req.session.dataCardBikeVar);
+  // -> undefined
 
-  if(req.session.dataCardBikeVar == undefined) {
+  if (req.session.dataCardBikeVar == undefined) {
     req.session.dataCardBikeVar = []
   }
-  res.render('index', { title: 'BikeShop - Home', dataBikeEJS : dataBikeVar });
+
+  res.render('index', { title: 'BikeShop - Home', dataBikeEJS: dataBikeVar, dataCardBikeEJS: req.session.dataCardBikeVar });
 });
 
 
 //Shop
-router.get('/shop', function(req, res, next) {
+router.get('/shop', function (req, res, next) {
 
   var alreadyExist = false;
 
-  for(var i = 0; i< req.session.dataCardBikeVar.length; i++){
-    if(req.session.dataCardBikeVar[i].name == req.query.bikeNameFromFront){
-      req.session.dataCardBikeVar[i].quantity = Number(req.session.dataCardBikeVar[i].quantity) + 1;
+  // On va passer dans cette boucle en premier :
+  // (Si le vélo existe déjà dans le shop, on rajoute 1)
+  for (var i = 0; i < req.session.dataCardBikeVar.length; i++) {
+    if (req.session.dataCardBikeVar[i].name == req.query.bikeNameFromFront) {
+      req.session.dataCardBikeVar[i].quantity += 1;
+      // req.session.dataCardBikeVar[i].quantity = Number(req.session.dataCardBikeVar[i].quantity) + 1;
+      
+      //on met le booléen à true, du coup, pas besoin d'aller dans la boucle suivante.
       alreadyExist = true;
     }
   }
 
-  if(alreadyExist == false){
+  // Si le vélo n'existe pas encore dans le shop, on passe dans cette boucle :
+  if (alreadyExist == false) {
     req.session.dataCardBikeVar.push({
       name: req.query.bikeNameFromFront,
       url: req.query.bikeUrlFromFront,
@@ -42,21 +51,21 @@ router.get('/shop', function(req, res, next) {
     })
   }
 
-  res.render('shop', {dataCardBikeEJS : req.session.dataCardBikeVar});
+  res.render('shop', { dataCardBikeEJS: req.session.dataCardBikeVar });
 });
 
 //Delete bike
-router.get('/delete-shop', function(req, res) {
-  req.session.dataCardBikeVar.splice(req.query.position, 1)    
-  res.render('shop', { dataCardBikeEJS : req.session.dataCardBikeVar });
+router.get('/delete-shop', function (req, res) {
+  req.session.dataCardBikeVar.splice(req.query.position, 1)
+  res.render('shop', { dataCardBikeEJS: req.session.dataCardBikeVar });
 })
 
 //Update shop
-router.post('/update-shop', function(req, res) {
+router.post('/update-shop', function (req, res) {
   let position = req.body.position
   let newQuantity = req.body.quantity
-  req.session.dataCardBikeVar[position].quantity = newQuantity 
-  res.render('shop', { dataCardBikeEJS : req.session.dataCardBikeVar });
+  req.session.dataCardBikeVar[position].quantity = newQuantity
+  res.render('shop', { dataCardBikeEJS: req.session.dataCardBikeVar });
 })
 
 module.exports = router;
